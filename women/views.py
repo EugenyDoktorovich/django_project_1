fdc61ab668ec8f0ba9a459637efbb365838935cd
Crystal_Pandemonium <jekdoktorov@yandex.ru>
2022-07-15 12:18:16 +0300

    Add lesson 15
    Add lesson about View classes

--- women/views.py
+++ women/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
 from .forms import *
 from .models import *
@@ -11,7 +13,25 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
 ]
 
 
-def index(request):
+class WomenHome(ListView):
+    model = Women
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list = None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Главная страница'
+        context['cat_selected'] = 0
+        return context
+
+    
+    def get_queryset(self):
+        return Women.objects.filter(is_published=True)
+
+
+
+""" def index(request):
     posts = Women.objects.all()
 
     context = {
@@ -21,13 +41,25 @@ def index(request):
         'cat_selected': 0,
     }
 
-    return render(request, 'women/index.html', context=context)
+    return render(request, 'women/index.html', context=context) """
 
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
 
-def addpage(request):
+class AddPage(CreateView):
+    form_class = AddPostForm
+    template_name = 'women/addpage.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list = None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Добавление статьи'
+        return context
+
+
+""" def addpage(request):
     if request.method == 'POST':
         form = AddPostForm(request.POST, request.FILES)
         if form.is_valid():
@@ -37,7 +69,7 @@ def addpage(request):
 
     else:
         form = AddPostForm()
-    return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
+    return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'}) """
 
 def contact(request):
     return HttpResponse("Обратная связь")
@@ -49,7 +81,7 @@ def login(request):
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
-def show_post(request, post_slug):
+""" def show_post(request, post_slug):
     post = get_object_or_404(Women, slug=post_slug)
 
     context = {
@@ -59,9 +91,42 @@ def show_post(request, post_slug):
         'cat_selected': post.cat_id,
     }
 
-    return render(request, 'women/post.html', context=context)
+    return render(request, 'women/post.html', context=context) """
 
-def show_category(request, cat_id):
+class ShowPost(DetailView):
+    model = Women
+    template_name = 'women/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list = None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = context['post']
+        return context
+
+
+class WomenCategory(ListView):
+    model = Women
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+
+    def get_queryset(self):
+        return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
+
+    def get_context_data(self, *, object_list = None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
+        context['cat_selected'] = context['posts'][0].cat_id
+        return context
+
+    
+
+
+""" def show_category(request, cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
 
     if len(posts) == 0:
@@ -74,5 +139,5 @@ def show_category(request, cat_id):
         'cat_selected': cat_id,
     }
 
-    return render(request, 'women/index.html', context=context)
+    return render(request, 'women/index.html', context=context) """
 
